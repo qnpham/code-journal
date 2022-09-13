e023a29c0935a7469df2b8a0fdff019b2bb086e3
Quan Pham <mockanterr@gmail.com>
2022-09-13 23:59:13 +0000

    defined function that returns li dom tree #5

--- js/main.js
+++ js/main.js
@@ -22,3 +22,39 @@ $form.addEventListener('submit', function (event) {
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
 });
+
+function createDom(entry) {
+  var li = document.createElement('li');
+
+  function makeChild(tagName, attr, attrValue) {
+    var element = document.createElement(tagName);
+    element.setAttribute(attr, attrValue);
+    return element;
+  }
+
+  var row = makeChild('div', 'class', 'row');
+  li.appendChild(row);
+
+  var columnHalf = makeChild('div', 'class', 'column-half');
+  row.appendChild(columnHalf);
+
+  var imgContainer = makeChild('div', 'class', 'entry-image-container');
+  columnHalf.appendChild(imgContainer);
+
+  var img = makeChild('img', 'src', entry.url);
+  imgContainer.appendChild(img);
+
+  var columnHalf2 = makeChild('div', 'class', 'column-half');
+  row.appendChild(columnHalf2);
+
+  var h4 = document.createElement('h4');
+  h4.textContent = entry.title;
+  columnHalf2.appendChild(h4);
+
+  var p = document.createElement('p');
+  p.textContent = entry.notes;
+  columnHalf2.appendChild(p);
+
+  return li;
+}
+createDom();
